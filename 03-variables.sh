29762011d29d3f050d78880b1880a3ebc9fb34f3
prasanth <mprasanth.dev@gmail.com>
2025-11-17 12:53:01 +0530

    message1

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learing recemntly?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"

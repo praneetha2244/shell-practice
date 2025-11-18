97f60d49425b08358d7126d478745b189e38ca04
prasanth <mprasanth.dev@gmail.com>
2025-11-18 11:16:56 +0530

    variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
+
+START_TIME=$(date +%s)
 
 echo "Timestamp executed: $DATE"
+
+sleep 10 &
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(END_TIME-$START_TIME)
+
+echo "Script excuted in: $TOTAL_TIME Seconds"

11ffe2c9632f46682d6cd1fa8933b3ad901e2f8d
prasanth <mprasanth.dev@gmail.com>
2025-11-20 11:34:40 +0530

    variables

---
+++ 15.colors.sh
@@ -0,0 +1,47 @@
+#!bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0    
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args}
+
+if [ $? -ne 0 ]; then
+    echo -e "ERRORInstalling $2 ... $R FAILURE $N"
+    exit 1
+    
+else
+    echo -e "Installing $2 ... $G is SUCCESS $N"
+fi   
+}     
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+   dnf install mysql -y
+   VALIDATE $? "MySql"
+else
+    echo -e "MySql already exist ... $Y SKIPPING $N"
+fi
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "NGINX"
+else
+    echo -e "Nginx already exist ... $Y SKIPPING $N"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? :"python3"
+else
+    echo -e "Python3 already exist ... $Y SKIPPING $N"
+fi

19b9d10954b12429197388716843f29845e15500
prasanth <mprasanth.dev@gmail.com>
2025-11-27 22:10:12 +0530

    session-6

--- 19-set.sh
+++ 19-set.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+error(){
+    echo "There is an error"
+}
+
+trap error ERR
+
 echo "Hello.."
 echo "Before error.."
 ccaffjl:dnf   # invalid command → prints error but script continues
